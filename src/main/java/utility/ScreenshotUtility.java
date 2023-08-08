a4e92c8ec27b815c3252c81382b1ceffd6794737
Kajal Miglani <kajal.miglani@nagarro.com>
2023-08-09 04:46:54 +0530

    deleted sysouts

--- src/main/java/utility/ScreenshotUtility.java
+++ src/main/java/utility/ScreenshotUtility.java
@@ -17,15 +17,12 @@ public class ScreenshotUtility {
 			 String errorDescription = result.getThrowable().getMessage().substring(0,50);
 	        try {
 	            String screenshotName = String.format("%s_%s.png", testCaseName, errorDescription.replaceAll("[^a-zA-Z0-9.-]", "_"));
-	            System.out.println(screenshotName);
 	            File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 	            File destFile = new File("screenshots", screenshotName);
 	            FileUtils.copyFile(screenshotFile,destFile);
-	            System.out.printf("Screenshot saved: %s%n", destFile.getAbsolutePath());            
-	            System.out.println("Screenshot saved: "+ screenshotName);
+	            
 	        } catch (Exception e) {
-	            System.err.println("Failed to save screenshot:"+ e.getMessage());
-	            e.printStackTrace();
+	          e.printStackTrace();
 	        }
 		 }
 	    }
