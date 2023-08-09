e43c09bb7eb7b135f29bcea1bdfb56defd3b1ff3
Kajal Miglani <kajal.miglani@nagarro.com>
2023-08-09 06:20:03 +0530

    adding code to check fix

--- src/main/java/utility/PropertyFileReader.java
+++ src/main/java/utility/PropertyFileReader.java
@@ -9,32 +9,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PropertyFileReader {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ScreenshotUtility.class);
-	 public static Properties readProperty(String fileName) throws IOException{
-	      FileInputStream fis = null;
-	      Properties prop = null;
-	      try {
-	         fis = new FileInputStream(fileName);
-	         prop = new Properties();
-	         prop.load(fis);
-	      } catch(FileNotFoundException ex) {
-	    	  logger.error("Error Occured", ex);
-	      } catch(IOException e) {
-	    	  logger.error("Error Occured", e);
-	      } finally {
-	    	  if(fis!=null) {
-	    	  try {
-	         fis.close();
-	    	  }
-	    	  catch(FileNotFoundException ex)
-	    	  {
-	    	 logger.error("Error Occured", e);
-	    	  }
-	    	  }
-	      }
-	      return prop;
-	   }
 
+	private static final Logger logger = LoggerFactory.getLogger(ScreenshotUtility.class);
+
+	public static Properties readProperty(String fileName) throws IOException {
+		FileInputStream fis = null;
+		Properties prop = null;
+		try {
+			fis = new FileInputStream(fileName);
+			prop = new Properties();
+			prop.load(fis);
+		} catch (FileNotFoundException ex) {
+			logger.error("Error Occured", ex);
+		} catch (IOException e) {
+			logger.error("Error Occured", e);
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (FileNotFoundException er) {
+					logger.error("Error Occured", er);
+				}
+			}
+		}
+		return prop;
+	}
 
 }
