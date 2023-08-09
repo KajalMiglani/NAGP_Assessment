404fab9a5034da6168b875e48cfb82d31cc6300b
Kajal Miglani <kajal.miglani@nagarro.com>
2023-08-09 06:37:38 +0530

    removing constant

--- src/main/java/utility/Constant.java
+++ src/main/java/utility/Constant.java
@@ -5,34 +5,15 @@ public class Constant {
 	public static final String DUMMY_EMAIL = "DUMMY_EMAIL";
 	public static final String DUMMY_USERNAME = "DUMMY_USERNAME";
 	public static final String SEARCH_POSITIVE = "POSITIVE_SEARCH_ITEM";
-	
-	public static final String REGISTER_PASSWORD = "REGISTER_PASSWORD";
-	public static final String REGISTER_EMAIL = "REGISTER_EMAIL";
-	public static final String REGISTER_LNAME = "REGISTER_LNAME";
-	public static final String REGISTER_FNAME = "REGISTER_FNAME";
-	
-	
 	public static final String SEARCH_NEGATIVE = "NEGATIVE_SEARCH_ITEM";
 	public static final String SEARCH_INVALID = "INVALID_SEARCH_ITEM";
-	
-
-	public static final String COMPARE_ITEM1 = "COMPARE_ITEM1";
-	public static final String COMPARE_ITEM2 = "COMPARE_ITEM2";
-	
-	
 	public static final String ADD_CART_ITEM = "ADD_CART_ITEM";
 	public static final String CART_QTY="CART_QTY";
-	
 	public static final String GLOBAL_CONFIG_PROPERTIES_PATH = "/src/test/resources/globalConfig.properties";
 	public static final String TEST_DATA_PATH = "/src/test/resources/testData.properties";
 	public static final String USER_DIR = "user.dir";
-	
 	public static final String CART_EDIT_QTY="CART_EDIT_QTY";
 	public static final String SHIPPING_TYPE = "Table Rate";
-	
-	public static final String VERIFIED_ORDER_NUMBER = "Verified Order Number generated: ";
-	public static final String THANK_YOU_FOR_YOUR_PURCHASE = "Thank you for your purchase!";
-	
 	public static final String SEARCH_RESULTS_FOR = "Search results for:";
 	public static final String SEARCH_RESULTS_DISPLAYED_ON_SCREEN_FOR_ITEM = "Search Results displayed on screen for item: ";
 	public static final String MESSAGE_DISPLAYED_YOUR_SEARCH_RETURNED_NO_RESULTS = "Message Displayed : Your search returned no results.";
@@ -41,57 +22,33 @@ public class Constant {
 	public static final String YOUR_SEARCH_RETURNED_NO_RESULTS = "Your search returned no results.";
 	public static final String SEARCH_MESSAGE_INVALID_ITEM = "When user try to search a item by giving less than 3 words, Message Displayed : Minimum Search query length is 3";
 	public static final String SEARCH_MESSAGE_UNAVAILABLE_ITEM = "When user try to search a unavailable item, Message Displayed : Your search returned no results";
-
-
-	public static final String PASSWORD2 = " password- ";
-	public static final String EMAIL2 = " email- ";
-	public static final String LAST_NAME = " lastName- ";
-	public static final String ENTERED_NEW_USER_DETAILS_FIRST_NAME = "Entered New User Details. FirstName- ";
-	public static final String CREATE_ACCOUNT_PAGE_HEADING = "Create New Customer Account";
-	
-	
 	public static final String VERIFIED_LOGGED_IN_EMAIL = "Verified logged in Email ";
 	public static final String LOGGED_IN_EMAIL_ID = " Logged In Email ID : ";
 	public static final String MY_ACCOUNT_PAGE_HEADING = "My Account";
-
-	
 	public static final String PASSWORD3 = " password: ";
 	public static final String ENTERED_LOG_IN_DETAILS_EMAIL = " Entered log in Details. Email: ";
-	
-	public static final String DISPLAYED_IN_WISHLIST = " displayed in wishlist";
-	public static final String HAS_BEEN_ADDED_TO_YOUR_WISH_LIST = " has been added to your Wish List.";
-
-	
 	public static final String LOGGED_IN_USER_NAME = "Logged In UserName : ";
 	public static final String HOME = "Home";
 	public static final String VERIFIED_LOGGED_IN_USER = "Verified Logged In User: ";
-
-	
 	public static final String VERIFIED_IN_PROGRESS_LABEL = "Verified In Progress Label ";
 	public static final String IN_PROGRESS_LABEL_IS_DISPLAYED = " - In progress Label is Displayed";
 	public static final String TELEPHONE_NUMBER_VERIFIED = "Telephone Number Verified : ";
 	public static final String CITY_VERIFIED = "City Verified : ";
 	public static final String STREET_NAME_VERIFIED = "Street Name Verified : ";
 	public static final String COMPANY_NAME_VERIFIED = "Company Name Verified : ";
-	public static final String ADDRESS = "Address: ";
 	public static final String FIXED = "Fixed";
 	public static final String VERIFIED_ORDER_SUMMARY = "verified order summary: ";
 	public static final String IS_DISPLAYED = " is Displayed";
 	public static final String ORDER_SUMMARY_ITEM_COLLAPSED = "orderSummary Item Collapsed";
 	public static final String SHIPPING = "Shipping";
 	public static final String REVIEW_PAYMENTS = "Review & Payments";
-	public static final String DUMMY_ACCOUNT_ADDRESS = "DUMMY_ADDRESS";
-	
 	public static final String TO_YOUR = " to your ";
 	public static final String YOU_ADDED = "You added ";
 	public static final String DISPLAYED_IN_CART = " displayed in cart";
 	public static final String VALUE = "value";
 	public static final String ITEM_QUANTITY_EDITED = "Verified Item Quantity Edited";
-
-
 	public static final String ADDRESS2 = "Address: ";
 	public static final String ADDRESS_ADDED = "Address added";
-	
 	public static final String HEADING_DISPLAYED = "Page Heading Displayed: ";
 	public static final String VERIFIED_PAGE_HEADING = "Verified Page Heading ";
 	public static final String TEST_CASE = "Test Case : ";
